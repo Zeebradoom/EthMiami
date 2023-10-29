406fc17a95e94c8d1e442ae9babbd3017b49b39e
Derrick Cui <cderrick126@gmail.com>
2023-10-29 02:00:44 -0400

    animations and tanium stuff

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 console.log("file");
 
+const button = document.getElementById("get-balance");
+const addressInput = document.getElementById("address");
+const balanceDiv = document.getElementById("balance");
+
+button.addEventListener("click", async () => {
+  const tatum = await TatumSDK.init({ network: Network.ETHEREUM });
+  const balance = await tatum.address.getBalance({
+    addresses: [addressInput.value],
+  });
+  const balanceData = balance.data.filter((asset) => asset.asset === "ETH")[0];
+  balanceDiv.textContent = `${balanceData.balance} ${balanceData.asset}`;
+});
+
+console.log("filestore");
+
 window.addEventListener("unlockProtocol.status", function (event) {
   // We hide all .unlock-content elements
   document.querySelector(".unlock-content").style.display = "none";
